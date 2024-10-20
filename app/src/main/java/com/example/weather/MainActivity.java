8480f06a01b25a84da71ce0ef4e11670a81fd27f
TomTheCattt <100224706+TomTheCattt@users.noreply.github.com>
2024-10-21 05:34:24 +0700

    update navigation từ home view sang city list view Edit: Việt Anh

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -29,14 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                navController.navigate(R.id.action_homeFragment_to_cityListFragment);
-            }
-        });
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
 
     @Override
--- app/src/main/java/ui/home/HomeFragment.java
+++ app/src/main/java/ui/home/HomeFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.example.weather.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
 
@@ -27,7 +30,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(v->{
+            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.action_homeFragment_to_cityListFragment);
+        });
+        return view;
     }
 
     @Override
