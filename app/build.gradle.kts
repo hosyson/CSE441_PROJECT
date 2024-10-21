86876636356766fa0fae152df49e351dce266f5f
duchai-do <haitong192@gmail.com>
2024-10-21 10:54:31 +0700

    duchaicm
    duchaicm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,10 @@ android {
 }
 
 dependencies {
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.squareup.picasso:picasso:2.71828")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
